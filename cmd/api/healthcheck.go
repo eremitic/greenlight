e00e5f65b88ce1a6b7f4b1838c729e8d42175af2
elvin <eremitic.elvin@gmail.com>
2021-12-12 16:26:08 +0800

    add writeJson helper n json health c3.2

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,14 +1,23 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 )
 
 // Declare a handler which writes a plain-text response with information about the
 // application status, operating environment and version.
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "status: available")
-	fmt.Fprintf(w, "environment: %s\n", app.config.env)
-	fmt.Fprintf(w, "version: %s\n", version)
+	// Create a map which holds the information that we want to send in the response.
+	data := map[string]string{
+		"status":      "available",
+		"environment": app.config.env,
+		"version":     version,
+	}
+
+	err := app.writeJSON(w, http.StatusOK, data, nil)
+
+	if err != nil {
+		app.logger.Println(err)
+		http.Error(w, "Server encountered problem and could not process request", http.StatusInternalServerError)
+	}
 }
