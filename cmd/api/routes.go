ffa14f5ff1da325e3175ade8f64d2e179a3e896f
elvin <eremitic.elvin@gmail.com>
2021-12-16 16:34:22 +0800

    error message json - c3.6

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -10,6 +10,15 @@ func (app *application) routes() *httprouter.Router {
 	// Initialize a new httprouter router instance.
 	router := httprouter.New()
 
+	// Convert the notFoundResponse() helper to a http.Handler using the
+	// http.HandlerFunc() adapter, and then set it as the custom error handler for 404
+	// Not Found responses.
+	router.NotFound = http.HandlerFunc(app.notFoundResponse)
+
+	// Likewise, convert the methodNotAllowedResponse() helper to a http.Handler and set
+	// it as the custom error handler for 405 Method Not Allowed responses.
+	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
+
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
 	router.HandlerFunc(http.MethodPost, "/v1/movies", app.createMovieHandler)
 	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.showMovieHandler)
