5382c28426a36a7230b6fe12a2c559e1f19ab774
eremitic <eremitic.elvin@hotmail.com>
2021-12-24 19:05:19 +0800

    handling partial update - c8.1

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -126,12 +126,12 @@ func (app *application) updateMovieHandler(w http.ResponseWriter, r *http.Reques
 		return
 	}
 
-	// Declare an input struct to hold the expected data from the client.
+	// Use pointers for the Title, Year and Runtime fields.
 	var input struct {
-		Title   string       `json:"title"`
-		Year    int32        `json:"year"`
-		Runtime data.Runtime `json:"runtime"`
-		Genres  []string     `json:"genres"`
+		Title   *string       `json:"title"`   // This will be nil if there is no corresponding key in the JSON.
+		Year    *int32        `json:"year"`    // Likewise...
+		Runtime *data.Runtime `json:"runtime"` // Likewise...
+		Genres  []string      `json:"genres"`  // We don't need to change this because slices already have the zero-value nil.
 	}
 
 	// Read the JSON request body data into the input struct.
@@ -141,12 +141,26 @@ func (app *application) updateMovieHandler(w http.ResponseWriter, r *http.Reques
 		return
 	}
 
-	// Copy the values from the request body to the appropriate fields of the movie
-	// record.
-	movie.Title = input.Title
-	movie.Year = input.Year
-	movie.Runtime = input.Runtime
-	movie.Genres = input.Genres
+	// If the input.Title value is nil then we know that no corresponding "title" key/
+	// value pair was provided in the JSON request body. So we move on and leave the
+	// movie record unchanged. Otherwise, we update the movie record with the new title
+	// value. Importantly, because input.Title is a now a pointer to a string, we need
+	// to dereference the pointer using the * operator to get the underlying value
+	// before assigning it to our movie record.
+	if input.Title != nil {
+		movie.Title = *input.Title
+	}
+
+	// We also do the same for the other fields in the input struct.
+	if input.Year != nil {
+		movie.Year = *input.Year
+	}
+	if input.Runtime != nil {
+		movie.Runtime = *input.Runtime
+	}
+	if input.Genres != nil {
+		movie.Genres = input.Genres // Note that we don't need to dereference a slice.
+	}
 
 	// Validate the updated movie record, sending the client a 422 Unprocessable Entity
 	// response if any checks fail.
