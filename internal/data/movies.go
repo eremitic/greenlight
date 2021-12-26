5ac469836610ecde379434971e9bddb3be75954f
eremitic <eremitic.elvin@hotmail.com>
2021-12-26 16:50:24 +0800

    pagination - c9.7

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -203,14 +203,16 @@ func (m MovieModel) GetAll(title string, genres []string, filters Filters) ([]*M
         FROM movies
         WHERE (to_tsvector('simple', title) @@ plainto_tsquery('simple', $1) OR $1 = '') 
         AND (genres @> $2 OR $2 = '{}')     
-        ORDER BY %s %s, id ASC`, filters.sortColumn(), filters.sortDirection())
+        ORDER BY %s %s, id ASC
+				LIMIT $3 OFFSET $4`, filters.sortColumn(), filters.sortDirection())
 
 	// Create a context with a 3-second timeout.
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
 
 	// Pass the title and genres as the placeholder parameter values.
-	rows, err := m.DB.QueryContext(ctx, query, title, pq.Array(genres))
+	rows, err := m.DB.QueryContext(ctx, query, title, pq.Array(genres), filters.limit(), filters.offset())
+
 	if err != nil {
 		return nil, err
 	}
