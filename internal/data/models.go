14a1e0648fde8f92831e58d38abc408daf43ebb7
eremitic <eremitic.elvin@hotmail.com>
2021-12-28 08:42:35 +0800

    setup user model - c13.2

--- internal/data/models.go
+++ internal/data/models.go
@@ -16,6 +16,7 @@ var (
 // like a UserModel and PermissionModel, as our build progresses.
 type Models struct {
 	Movies MovieModel
+	Users  UserModel // Add a new Users field.
 }
 
 // For ease of use, we also add a New() method which returns a Models struct containing
@@ -23,5 +24,6 @@ type Models struct {
 func NewModels(db *sql.DB) Models {
 	return Models{
 		Movies: MovieModel{DB: db},
+		Users:  UserModel{DB: db}, // Initialize a new UserModel instance.
 	}
 }
