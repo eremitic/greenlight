3af79e7f6f7596e1373900debdeaa4ef3c55ef0c
eremitic <eremitic.elvin@hotmail.com>
2022-01-06 21:07:53 +0800

    generate auth token - c16.2

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -30,6 +30,8 @@ func (app *application) routes() http.Handler {
 	router.HandlerFunc(http.MethodPost, "/v1/users", app.registerUserHandler)
 	router.HandlerFunc(http.MethodPut, "/v1/users/activated", app.activateUserHandler)
 
+	router.HandlerFunc(http.MethodPost, "/v1/tokens/authentication", app.createAuthenticationTokenHandler)
+
 	// Wrap the router with the panic recovery middleware.
 	return app.recoverPanic(app.rateLimit(router))
 
