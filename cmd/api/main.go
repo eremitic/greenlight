378c3002d74ffd44e1ef1a4e0c8bb1ea880f45c8
eremitic <eremitic.elvin@hotmail.com>
2022-01-13 22:11:34 +0800

    create custom metrics - c19.2

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"context"      // New import
 	"database/sql" // New import
+	"expvar"
 	"flag"
 	"os"
+	"runtime"
 	"strings"
 	"sync"
 	"time"
@@ -111,6 +113,25 @@ func main() {
 	defer db.Close()
 
 	logger.PrintInfo("database connection pool established", nil)
+
+	// Publish a new "version" variable in the expvar handler containing our application
+	// version number (currently the constant "1.0.0").
+	expvar.NewString("version").Set(version)
+
+	// Publish the number of active goroutines.
+	expvar.Publish("goroutines", expvar.Func(func() interface{} {
+		return runtime.NumGoroutine()
+	}))
+
+	// Publish the database connection pool statistics.
+	expvar.Publish("database", expvar.Func(func() interface{} {
+		return db.Stats()
+	}))
+
+	// Publish the current Unix timestamp.
+	expvar.Publish("timestamp", expvar.Func(func() interface{} {
+		return time.Now().Unix()
+	}))
 
 	app := &application{
 		config: cfg,
